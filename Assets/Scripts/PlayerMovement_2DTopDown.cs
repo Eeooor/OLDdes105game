318c120150637a03695bf5bd95dc3c1299ee7e9d
Eeooor <kahanovlior@yahoo.com>
2023-10-31 15:23:31 +0000

    Updated PlayerMovement
    Specified intended game type use
    Should be reusable for future projects :)

---
+++ Assets/Scripts/PlayerMovement_2DTopDown.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_2DTopDown : MonoBehaviour
+{
+    // IMPORTANT NOTES FOR REUSE!
+    // REQUIRES RIDIDBODY2D TO BE ATTATCHED TO GAMEOBJECT!
+
+
+    //----------------------------------------------
+
+    Vector3 PlayerMovement; //Defines vector for player movement
+
+    Rigidbody2D Player_Rigidbody; //Defines Ridgidbody
+
+    //----------------------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set up on start: sets PlayerMovement to a new vector and gets ridgidbody component of attached GameObject
+        PlayerMovement = new Vector3();
+        Player_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInputs();
+    }
+
+    void CheckInputs()
+    {
+        PlayerMovement.x = Input.GetAxisRaw("Horizontal");
+        PlayerMovement.y = Input.GetAxisRaw("Vertical");
+
+        // Directly assigns values to Attached Ridgidbody
+        Player_Rigidbody.velocity = PlayerMovement;
+    }
+
+}
